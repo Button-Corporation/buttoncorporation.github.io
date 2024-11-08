6d9ebce7f112a25a2d5cd42d2a3fcfd91801fe3f
Owen CYMK <42657486+OwenCMYK@users.noreply.github.com>
2024-11-08 15:32:46 -0330

    Fixed up some of Tregi's stuff
    - Made the volume sliders align better and no longer cause scroll bar
    - Reworked the way slots are done to be more like how actual slot machines work (also just a better and more scalable solution)
    - Moved peepo check to CheckWinner function
    - Removed unnecessary inputs to Unclick function
    - Commented all the slots code so Lyra can wrap her cute little brain around it

--- slots.js
+++ slots.js
@@ -10,60 +10,90 @@ var musicSlider;
 var sfxSlider;
 
 function DoSpin() {
+    // Plays the music
+    // This does nothing if the music is already playing
+    // - Owen
     Music.play()
+
+    // This disables the button
+    // - Owen
     document.getElementsByClassName("slotbutton")[0].disabled = true;
     document.getElementsByClassName("slotbutton")[0].classList.toggle("pressed")
     document.getElementsByClassName("slotbutton")[0].textContent = "Pressed"
 
-    var slotImages = ["1.png", "2.png", "3.png", "4.png"];
-    var r1 = Math.floor((Math.random() * 10));
-    var r2 = Math.floor((Math.random() * 10));
-    var r3 = Math.floor((Math.random() * 10));
-    if (r1 === 0 && r2 === 0 && r3 === 0) {/*do something special*/}; 
-    var slotImage1 = (r1 == 0) ? 3 : (r1 % 3 == 0) ? 0 : (r1 % 3 == 1) ? 1 : 2;
-    var slotImage2 = (r2 == 0) ? 3 : (r2 % 3 == 0) ? 0 : (r2 % 3 == 1) ? 1 : 2;
-    var slotImage3 = (r3 == 0) ? 3 : (r3 % 3 == 0) ? 0 : (r3 % 3 == 1) ? 1 : 2;
+    // The actual pattern that the slot machine uses to decide odds
+    // Something appearing twice means it's twice as likely
+    // Slot images are named by their number (0 is Peepo)
+    // - Owen
+    var SlotPattern = [0,1,1,1,2,2,2,3,3,3]
+
+    // Picks a random number from the slot pattern
+    // - Owen
+    var slotImage1 = SlotPattern[Math.floor((Math.random() * SlotPattern.length))];
+    var slotImage2 = SlotPattern[Math.floor((Math.random() * SlotPattern.length))];
+    var slotImage3 = SlotPattern[Math.floor((Math.random() * SlotPattern.length))];
+    
+
+    // This sets all the slot images to the rolling gif
+    // - Owen
     document.getElementById('SlotImage1').src = "Mystery.gif";
     document.getElementById('SlotImage2').src = "Mystery.gif";
     document.getElementById('SlotImage3').src = "Mystery.gif";
     Spin.play()
 
     setTimeout(function () {
-        document.getElementById('SlotImage1').src = slotImages[slotImage1];
+        // Sets the first slot to reveal after 500 ms
+        // - Owen
+        document.getElementById('SlotImage1').src = slotImage1+".png";
         Click.play()
     }, 500);
     setTimeout(function () {
-        document.getElementById('SlotImage2').src = slotImages[slotImage2];
+        // Sets the second slot to reveal after 1000 ms
+        // - Owen
+        document.getElementById('SlotImage2').src = slotImage2+".png";
         Click.play()
     }, 1000);
     setTimeout(function () {
-        document.getElementById('SlotImage3').src = slotImages[slotImage3];
+        // Sets the third slot to reveal after 1500 ms
+        // - Owen
+        document.getElementById('SlotImage3').src = slotImage3+".png";
         Click.play()
         CheckWinner(slotImage1, slotImage2, slotImage3);
     }, 1500);
     setTimeout(function () {
-        Unclick(slotImage1, slotImage2, slotImage3);
+        // Unpresses the button after 2000 ms
+        // - Owen
+        Unclick();
         UnclickSound.play()
     }, 2000);
 
 }
 
 function CheckWinner(x, y, z) {
+    // Checks for 3 Peepos
+    // - Owen
+    if (slotImage1 === 0 && slotImage2 === 0 && slotImage3 === 0) {/*do something special*/}; 
+
+    // Checks if you win at all
+    // - Owen
     if (x == y && x == z) {
-        // alert('YOU WIN!');
         Win.play()
         NumberOfWins = NumberOfWins + 1
         document.getElementsByClassName("wintext")[0].textContent = "Wins: " + NumberOfWins
-    } else {}
+    }
 }
 
 function Unclick() {
+    // Unclicks the button
+    // - Owen
     document.getElementsByClassName("slotbutton")[0].disabled = false;
     document.getElementsByClassName("slotbutton")[0].classList.toggle("pressed")
     document.getElementsByClassName("slotbutton")[0].textContent = "Button"
 }
 
 window.addEventListener('load', function () {
+    // Does the music and sound sliders
+    // - Owen
     musicSlider = document.getElementById('musicSlider');
     musicSlider.addEventListener('input', function() {
         Music.volume = this.value;
@@ -78,6 +108,8 @@ window.addEventListener('load', function () {
     });
 
     document.body.onkeyup = function (e) {
+        // Makes the space button also roll the slots
+        // - Owen
         if (e.key == " " ||
             e.code == "Space" ||
             e.keyCode == 32
