dc9db83320f1d0e488b3354b58dd900d3a7dea81
Owen CYMK <42657486+OwenCMYK@users.noreply.github.com>
2024-11-09 20:45:10 -0330

    Nerfing peepo again because expected value is WAY TOO HIGH

---
+++ Expected Value Calculator.py
@@ -0,0 +1,19 @@
+def CalculateExpectedValue(SlotPattern,Spins=30*10): #Defaults to 10 minutes
+	Value=0
+	CurrentSpins=0
+	for x in SlotPattern:
+		for y in SlotPattern:
+			for z in SlotPattern:
+				if x==y and y==z:
+					#Regular win
+					Value+=1
+				if x==0 and y==0 and z==0:
+					#Peepo win
+					Value+=99
+				CurrentSpins+=1
+	return Value*Spins/CurrentSpins
+
+print("Regular value over 10 minutes:")
+print(CalculateExpectedValue([0,1,1,1,2,2,2,3,3,3]))
+print("Peepo2x value over 10 minutes:")
+print(CalculateExpectedValue([0,0,1,1,1,2,2,2,3,3,3]))
